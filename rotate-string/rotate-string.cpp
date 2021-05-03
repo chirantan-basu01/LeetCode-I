65611b347e3b4b69540535b1567339bbd969fe4b
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-05-03 21:05:36 +0530

    Time: 4 ms (20.27%), Space: 6.8 MB (16.09%) - LeetHub

---
+++ rotate-string/rotate-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool rotateString(string A, string B) {
+        if(A.size()!=B.size())
+            return false;
+        if(A==B)
+            return true;
+        int len=B.size();
+        while(len--){
+            if(A==B)
+                return true;
+            A=A.substr(1)+A[0];
+        }
+        return false;
+    }
+};
