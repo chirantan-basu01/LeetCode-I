8ac9216b79fc45f9e1ad183e8e0e85bf4c759385
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-05-31 17:25:30 +0530

    Time: 60 ms (88.20%), Space: 21.2 MB (47.85%) - LeetHub

---
+++ 3sum/3sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>>res;
+        sort(nums.begin(),nums.end());
+        
+        for(int i=0;i<nums.size();i++){
+            int target=-nums[i];
+            int front=i+1;
+            int back=nums.size()-1;
+            
+            while(front<back){
+                int sum=nums[front]+nums[back];
+                
+                if(sum<target){
+                    front++;
+                }
+                else if(sum>target){
+                    back--;
+                }
+                else{
+                    vector<int>triplet={nums[i],nums[front],nums[back]};
+                    res.push_back(triplet);
+                    
+                    while(front<back and nums[front]==triplet[1])
+                        front++;
+                    while(front<back and nums[back]==triplet[2])
+                        back--;
+                }
+            }
+            while(i+1 <nums.size() and nums[i]==nums[i+1])
+                i++;
+        }
+        return res;
+    }
+};
