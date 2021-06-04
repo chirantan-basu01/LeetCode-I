60818fd721326ef21bbd80800e9a076941e5d737
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-06-04 20:02:48 +0530

    Time: 4 ms (80.33%), Space: 7.4 MB (23.86%) - LeetHub

---
+++ longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int>stk;
+        stk.push(-1);
+        int maxL=0;
+        for(int i=0;i<s.size();i++){
+            int t=stk.top();
+            if(t!=-1 and s[i]==')' and s[t]=='('){
+                stk.pop();
+                maxL=max(maxL,i-stk.top());
+            }
+            else{
+                stk.push(i);
+            }
+        }
+        return maxL;
+    }
+};
