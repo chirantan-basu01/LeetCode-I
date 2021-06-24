a93eb1f49079e7d3254f5e6cb1adcd6fd5607a44
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-06-24 11:13:32 +0530

    Time: 116 ms (64.05%), Space: 11.2 MB (74.64%) - LeetHub

---
+++ partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+        }
+        if(sum%2!=0)
+            return false;
+        sum=sum/2;
+        int n=nums.size();
+        
+        bool dp[n+1][sum+1];
+        
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<sum+1;j++){
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                   dp[i][j]=1;
+            }
+        }
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(nums[i-1]<=j)
+                    dp[i][j]=(dp[i-1][j-nums[i-1]] || dp[i-1][j]);
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        }
+        return dp[n][sum];
+    }
+};
