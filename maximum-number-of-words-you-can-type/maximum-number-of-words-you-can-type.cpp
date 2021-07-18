0fc2ce7cb7c8b03a56d4580b0e272aff9dac8aba
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-07-18 10:17:01 +0530

    Time: 9 ms (33.33%), Space: 6.6 MB (100.00%) - LeetHub

--- maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
+++ maximum-number-of-words-you-can-type/maximum-number-of-words-you-can-type.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int canBeTypedWords(string text, string bl) {
-        int cnt =0, res =0;
-	for (int i=0; i<text.length(); i++) 
-	{
-		for (int j=0; j<bl.length(); j++)
-		{
-			if (text[i]==bl[j]) 
-				cnt=1;
-		}
-		if (text[i]==' ' || i==text.length()-1)
-		{
-			if (cnt==0) res++;
-			else cnt=0;
-		}
-	}
-	return res;  
+        int res=0,cnt=0;
+        for(int i=0;i<text.length();i++){
+            for(int j=0;j<bl.length();j++){
+                if(text[i]==bl[j]){
+                    cnt=1;
+                }
+            }
+            if(text[i]==' '|| i==text.length()-1){
+                if(cnt==0)
+                    res++;
+                else
+                    cnt=0;
+            }
+        }
+        return res;
     }
 };
