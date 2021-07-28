659ed3f3df36ed65fe17840afe0fdcd4622b5cc8
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-07-28 20:30:42 +0530

    Time: 164 ms (84.05%), Space: 7.2 MB (79.43%) - LeetHub

---
+++ word-search/word-search.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    bool search(vector<vector<char>>& board,int i,int j,string &word,int idx,vector<vector<bool>>&visited){
+        if(idx==word.size()-1)
+            return true;
+        visited[i][j]=true;
+        
+        
+        if(i>0 and !visited[i-1][j] and board[i-1][j]==word[idx+1] and search(board,i-1,j,word,idx+1,visited))
+            return true;
+        
+        if(j>0 and !visited[i][j-1] and board[i][j-1]==word[idx+1] and search(board,i,j-1,word,idx+1,visited))
+            return true;
+        
+        if(i<board.size()-1 and !visited[i+1][j] and board[i+1][j]==word[idx+1] and search(board,i+1,j,word,idx+1,visited))
+            return true;
+        
+        if(j<board[0].size()-1 and !visited[i][j+1] and board[i][j+1]==word[idx+1] and search(board,i,j+1,word,idx+1,visited))
+            return true;
+        
+        
+        visited[i][j]=false;
+        return false;
+    }
+    
+    bool exist(vector<vector<char>>& board, string word) {
+        int r=board.size(),c=board[0].size();
+        vector<vector<bool>>visited(r,vector<bool>(c,false));
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(board[i][j]==word[0] and search(board,i,j,word,0,visited))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
