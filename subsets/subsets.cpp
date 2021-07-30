800f504b1de3e801404eb0450a54a9c9b1057b67
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-07-30 18:02:52 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (65.33%) - LeetHub

--- subsets/subsets.cpp
+++ subsets/subsets.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>subs={{}};
-        for(int num:nums){
-            int n=subs.size();
-            for(int i=0;i<n;i++){
-                subs.push_back(subs[i]);
-                subs.back().push_back(num);
-            }
-        }
+        vector<vector<int>>subs;
+        vector<int>sub;
+        subsets(nums,0,sub,subs);
         return subs;
+    }
+    
+    void subsets(vector<int>&nums,int i,vector<int>&sub,vector<vector<int>>&subs){
+        subs.push_back(sub);
+        for(int j=i;j<nums.size();j++){
+            sub.push_back(nums[j]);
+            subsets(nums,j+1,sub,subs);
+            sub.pop_back();
+        }   
     }
 };
