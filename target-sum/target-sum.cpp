aa5c6d479df3d72fd279e247a7db7c26403608a0
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-08-09 16:14:12 +0530

    Time: 1164 ms (13.22%), Space: 8.8 MB (98.61%) - LeetHub

--- target-sum/target-sum.cpp
+++ target-sum/target-sum.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
+    int count=0;
     int findTargetSumWays(vector<int>& nums, int target) {
-        int range=0,cz=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0)
-                cz++;
-            else
-                range+=nums[i];
-        }
-        
-        if(target>range)
-            return 0;
-        
-        int sum=range+target;
-        
-        if(sum%2==1)
-            return 0;
-        
-        sum=sum/2;
-        int n=nums.size();
-        int dp[n+1][sum+1];
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0)
-                    dp[i][j]=0;
-                if(j==0)
-                    dp[i][j]=1;
+        calculate(nums,target,0,0);
+        return count;
+    }
+    
+    void calculate(vector<int>& nums,int target,int idx,int sum){
+        if(idx==nums.size()){
+            if(sum==target){
+                count++;
             }
         }
-        
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if((nums[i-1]<=j) && (nums[i-1]!=0))
-                    dp[i][j]=dp[i-1][j]+dp[i-1][j-nums[i-1]];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
+        else{
+            calculate(nums,target,idx+1,sum+nums[idx]);
+            calculate(nums,target,idx+1,sum-nums[idx]);
         }
-        return pow(2,cz)*dp[n][sum];
     }
 };
