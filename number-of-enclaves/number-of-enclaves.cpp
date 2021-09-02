5b1eae39c839ce484a3d0c459d64f158ddaf8523
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-09-02 14:53:41 +0530

    Time: 75 ms (24.38%), Space: 21.8 MB (60.78%) - LeetHub

--- number-of-enclaves/number-of-enclaves.cpp
+++ number-of-enclaves/number-of-enclaves.cpp
@@ -3,11 +3,10 @@ public:
     int DFS(int i,int j,int m,int n,vector<vector<int>>& grid){
         if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0)
             return 0;
-        
         grid[i][j]=0;
-        return 1+DFS(i+1,j,m,n,grid)+DFS(i-1,j,m,n,grid)+DFS(i,j+1,m,n,grid)+DFS(i,j-1,m,n,grid);
+        return 1+DFS(i+1,j,m,n,grid)+DFS(i-1,j,m,n,grid)+
+            DFS(i,j+1,m,n,grid)+DFS(i,j-1,m,n,grid);
     }
-    
     int numEnclaves(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
