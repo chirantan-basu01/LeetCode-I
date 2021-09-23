42f8f162c80f2196ced782315edd6ff56dca5a0a
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-09-23 09:48:14 +0530

    Time: 564 ms (15.03%), Space: 17.9 MB (5.04%) - LeetHub

--- jump-game-ii/jump-game-ii.cpp
+++ jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
+    int solve(int idx,vector<int>& nums,vector<int>&dp){
+        
+        if(idx>=nums.size()-1)
+            return 0;
+        if(dp[idx]!=10001)
+            return dp[idx];
+        // unsigned int temp=0;
+        for(int i=1;i<=nums[idx];i++)
+            dp[idx]=min(dp[idx],1+solve(idx+i,nums,dp));
+        return dp[idx];
+    }
+    
     int jump(vector<int>& nums) {
         int n=nums.size();
-        int steps=0,start=0,end=0;
-        while(end<n-1){
-            steps++;
-            int maxend=end+1;
-            for(int i=start;i<=end;i++){
-                if(i+nums[i]>=n-1)
-                    return steps;
-                maxend=max(maxend,i+nums[i]);
-            }
-            start=end+1;
-            end=maxend;
-        }
-        return steps;
+        vector<int>dp(n+1,10001);
+        return solve(0,nums,dp);
     }
 };
