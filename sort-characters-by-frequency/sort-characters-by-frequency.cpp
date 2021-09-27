037d3820d21db5f9673f7c7531e7c799467b200d
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-09-27 11:04:53 +0530

    Time: 12 ms (79.43%), Space: 8.3 MB (79.68%) - LeetHub

---
+++ sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<char,int>ump;
+        string ans;
+        for(int i=0;i<s.size();i++){
+            ump[s[i]]++;
+        } 
+        
+        priority_queue<pair<int,char>>pq;
+        for(auto it:ump){
+            pq.push({it.second,it.first});
+        }
+        while(!pq.empty()){
+            pair<int,char>curr=pq.top();
+            pq.pop();
+            // cout<<curr.first<<" "<<curr.second;
+            int freq=curr.first;
+            while(freq>0){
+                ans.push_back(curr.second);
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
