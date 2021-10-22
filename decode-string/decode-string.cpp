be77454d62ea46a6bdf9d9553af17ba1b34b6f3f
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-10-22 09:51:12 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (54.35%) - LeetHub

---
+++ decode-string/decode-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    string repeat(string str,int times){
+        string res="";
+        
+        for(int i=0;i<times;i++){
+            res+=str;
+        }
+        return res;
+    }
+    
+    string decodeString(string s) {
+        int i=0;
+        while(i<s.size()){
+            if(s[i]!=']'){
+                i++;
+                continue;
+            }
+            
+            int j=i;
+            while(s[j]!='[')
+                j--;
+            
+            string letters_to_repeat=s.substr(j+1,i-j-1);
+            int k=j;
+            j--;
+            
+            while((j>0) and (isdigit(s[j])))
+                j--;
+            
+            if(j!=0) j++;
+            int times_to_repeat=stoi(s.substr(j,k-j));
+            
+            s.replace(j,i-j+1,repeat(letters_to_repeat,times_to_repeat));
+            i=letters_to_repeat.size()*times_to_repeat;
+        }
+        return s;
+    }
+};
