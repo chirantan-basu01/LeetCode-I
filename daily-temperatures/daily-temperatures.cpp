b1b39190113d6cff9bfe4c63c2031ef337bf481b
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-11-16 10:58:03 +0530

    Time: 144 ms (67.34%), Space: 88.9 MB (42.84%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n=temperatures.size();
+        
+        vector<int>ans(n);
+        stack<int>stk;
+        
+        for(int i=0;i<n;i++){
+            while(!stk.empty() and temperatures[stk.top()]<temperatures[i]){
+                ans[stk.top()]=i-stk.top();
+                stk.pop();
+            }
+            stk.push(i);
+        }
+        return ans;
+    }
+};
