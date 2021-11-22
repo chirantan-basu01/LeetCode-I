8344e1789b2e7337d50c982139d8673a1cef295d
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-11-22 16:47:11 +0530

    Time: 16 ms (38.53%), Space: 12.7 MB (84.97%) - LeetHub

---
+++ group-the-people-given-the-group-size-they-belong-to/group-the-people-given-the-group-size-they-belong-to.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
+        vector<vector<int>>ans;
+        map<int,vector<int>>mp;
+        
+        for(int i=0;i<groupSizes.size();i++){
+            mp[groupSizes[i]].push_back(i);
+            if(mp[groupSizes[i]].size()==groupSizes[i]){
+                ans.push_back(mp[groupSizes[i]]);
+                mp[groupSizes[i]].clear();
+            }
+        }
+        return ans;
+    }
+};
