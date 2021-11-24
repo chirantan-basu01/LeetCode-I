421846580bbd281d088b7db6a162fe329209d264
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-11-24 23:56:58 +0530

    Time: 15 ms (16.69%), Space: 13.5 MB (5.09%) - LeetHub

---
+++ best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int dp[100005][2];
+    
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
+        return bestTime(prices,0,true);
+    }
+    
+    int bestTime(vector<int>& prices,int currentDay,bool canBuy){
+        if(currentDay>=prices.size())
+            return 0;
+        
+       if(dp[currentDay][canBuy]!=-1)
+           return dp[currentDay][canBuy];
+        
+        int idle=bestTime(prices,currentDay+1,canBuy);
+        int buy=0;
+        int sell=0;
+        
+        if(canBuy){
+            buy=-prices[currentDay]+bestTime(prices,currentDay+1,false);
+            dp[currentDay][canBuy]=max(idle,buy);
+        }
+        else{
+            sell=prices[currentDay]+bestTime(prices,currentDay+2,true);
+            dp[currentDay][canBuy]=max(idle,sell);
+        }
+        return dp[currentDay][canBuy];
+    }
+};
