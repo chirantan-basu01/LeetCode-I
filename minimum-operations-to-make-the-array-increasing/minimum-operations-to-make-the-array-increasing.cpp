d21d299230e8b1555fea19a770f175f8c3c2ec2b
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-11-27 10:54:35 +0530

    Time: 23 ms (18.85%), Space: 15.7 MB (98.24%) - LeetHub

---
+++ minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]<=nums[i-1]){
+                count+=(nums[i-1]-nums[i]+1);
+                nums[i]=nums[i-1]+1;
+            }
+        }
+        return count;
+    }
+};
