4e3db6949ea894d8c8fcdb2458eaa0d860b115e4
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2021-12-01 17:35:44 +0530

    Time: 24 ms (28.35%), Space: 16.4 MB (25.94%) - LeetHub

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
+    int helper(int start,int end,bool turn,vector<int>& piles,vector<vector<int>>&dp){
+        //base condition
+        if(start>end)
+            return 0;
+        
+        //dp condition
+        if(dp[start][end]!=-1)
+            return dp[start][end];
+        
+        //alex turn
+        if(turn){
+            int rr1=piles[start]+helper(start+1,end,false,piles,dp);
+            int rr2=piles[end]+helper(start,end-1,false,piles,dp);
+            dp[start][end]=max(rr1,rr2);
+            return dp[start][end];
+        }
+        //lee turn
+            int rr1=piles[start]+helper(start+1,end,true,piles,dp);
+            int rr2=piles[end]+helper(start,end-1,true,piles,dp);
+            dp[start][end]=min(rr1,rr2);
+            return dp[start][end];
+    }
+    
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n=piles.size();
+        int start=0;
+        int end=n-1;
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        
+        return helper(0,n-1,1,piles,dp);
     }
 };
