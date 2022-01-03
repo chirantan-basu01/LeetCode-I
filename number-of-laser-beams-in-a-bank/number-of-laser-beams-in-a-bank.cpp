46b5cc66a9bb07635289eb508da366deced71425
Chirantan Basu <54743158+chirantan-basu01@users.noreply.github.com>
2022-01-03 09:59:00 +0530

    Time: 241 ms (16.67%), Space: 23.1 MB (33.33%) - LeetHub

--- number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
+++ number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        vector<int>nums;
-        int count=0;
+        int totalBeans = 0, curSecurity = 0, prevSecurity = 0;
         
-        for(int i=0;i<bank.size();i++){
-            count=0;
-            for(int j=0;j<bank[0].size();j++){
-                if(bank[i][j]=='1'){
-                    count++;
-                }                
+        for(string &floor: bank) {
+            curSecurity = count(floor.begin(), floor.end(), '1');
+            if(curSecurity) {
+                totalBeans += (curSecurity * prevSecurity);
+                prevSecurity = curSecurity;
             }
-            if(count>0)
-                nums.push_back(count);
         }
-        if(nums.size()<=1)
-            return 0;
-            
-        long long sum=0;
-        for(int i=0;i<nums.size()-1;i++){
-            sum+=(nums[i]*nums[i+1]);
-        }
-        return sum;
+        
+        return totalBeans;
     }
 };
